9954e594ba0ff663f2f0eb6e17b33da8faf75ac2
ClaudiaAF <180181@virtualwindow.co.za>
2021-07-19 03:41:07 -0700

    Initial Commit

---
+++ Alchemist/AlchemistApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlchemistApp.swift
+//  Alchemist
+//
+//  Created by Claudia Ferreira on 28/04/1400 AP.
+//
+
+import SwiftUI
+
+@main
+struct AlchemistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alchemist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alchemist
+//
+//  Created by Claudia Ferreira on 28/04/1400 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
