583e4a13a12ffecba0003670de26d5f6f2d2e02e
ClaudiaAF <180181@virtualwindow.co.za>
2021-07-23 01:58:17 -0700

    Splash Page Basic Layout Created

--- Alchemist/ContentView.swift
+++ Alchemist/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("LogoTransp")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 300, alignment: .top)
+                
+                //TODO: Replace with glowing text
+                Text("Alchemist")
+                    .font(.system(size: 60, weight: .black))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Text("Your Go-To Skyrim Alchemy Library")
+                    .font(.system(size: 20, weight: .regular))
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+            }
+        }
     }
 }
 
